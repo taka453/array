4d1e3e0bf5ceca6ef12f4d63d8208afa66b43602
sugu3 <teddy.picker.505@gmail.com>
2020-06-08 10:51:42 +0900

    first commit

---
+++ array/main.js
@@ -0,0 +1,22 @@
+// 配列
+
+// const members = ["田中","高橋","鈴木","佐藤"];
+// document.write(members[0]);
+
+// 配列の長さを調べる
+// document.write(members.length);
+
+// 配列に後で要素を追加する
+// members.push("山田");
+// document.write(members);
+
+// 連想配列
+
+// const members = {
+//     name: "田中",
+//     age: 20,
+//     gender: "女性",
+// }
+
+// document.write(members.age);
+
